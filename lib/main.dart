fb43d7c9d9092c410ebda2196c1c0ee4fcfb75cb
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-17 16:26:51 +0530

    Added the packages and API Key

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
   runApp(MyApp());
 }
+//FlutterConfig.get('API_KEY')
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,8 +31,26 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   GoogleMapController mapController;
+  String searchAddr;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    print(FlutterConfig.get('GOOGLE_MAPS_API_KEY'));
+    return Scaffold(
+      body: Stack(
+        children: [
+          // GoogleMap(
+          //   onMapCreated: onMapCreated,
+          //   //options: GoogleMapOptions(),
+          //   initialCameraPosition: CameraPosition(target:),
+          // ),
+        ],
+      ),
+    );
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
